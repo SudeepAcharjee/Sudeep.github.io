62f5f9b173281e1939bad9358f13e730fead2828
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2024-05-10 21:26:14 +0530

    Added

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,17 +1,48 @@
-import React from 'react';
-
-function WebPage() {
+import React from 'react'
+import Blog6 from './assests/images/blog-6.jpg'
+function Blog() {
   return (
-    <div className="webpage-container">
-      <iframe
-        src="" // Replace with the URL of the webpage you want to embed
-        title="Embedded Webpage"
-        width="100%"
-        height="800px"
-        frameBorder="0"
-      />
-    </div>
-  );
+    <>
+
+  <header>
+    <h2 className="h2 article-title">Blog</h2>
+  </header>
+  <section className="blog-posts">
+    <ul className="blog-posts-list">
+
+
+       {/* BLOG POST ITEM */}
+      <li className="blog-post-item">
+        <a href="#">
+          <figure className="blog-banner-box">
+            <img
+              src={Blog6}
+              alt="Design digest #79"
+              loading="lazy"
+            />
+          </figure>
+          <div className="blog-content">
+            <div className="blog-meta">
+              <p className="blog-category">Design</p>
+              <span className="dot" />
+              <time dateTime="2022-02-23">Fab 23, 2022</time>
+            </div>
+            <h3 className="h3 blog-item-title">Design digest #79</h3>
+            <p className="blog-text">
+              Optio cumque nihil impedit uo minus quod maxime placeat, velit
+              esse cillum.
+            </p>
+          </div>
+        </a>
+      </li>
+
+
+
+      
+    </ul>
+  </section>
+      </>
+  )
 }
 
-export default WebPage;
+export default Blog
--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -2,23 +2,15 @@ import React from 'react'
 
 function Contact() {
   return (
-    <>
-  <header>
-    <h2 className="h2 article-title">Contact</h2>
-  </header>
-  <section className="contact-form">
-    
+  
+     <div className="webpage-container">
       <iframe
-        src="https://forms.visme.co/formsPlayer/76nkg667-contact-us"
-        width={1000}
-        height={800}
-        loading="lazy"
+        src="https://forms.visme.co/formsPlayer/76nkg667-contact-us" // Replace with the URL of the webpage you want to embed
+        title="Embedded Webpage"
+       
       />
+    </div>
     
-  </section>
-
-
-    </>
   )
 }
 
