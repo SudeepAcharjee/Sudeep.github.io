55279406356ade594d3ad7cc4c3dda4f3c443d06
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2024-06-04 23:31:01 +0530

    Added

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChrome} from '@fortawesome/free-brands-svg-icons';
 
-// , faSass, faBootstrap, faGit, faNpm, faJs, faReact, faVuejs, faAngular, faFont, faFileCode, f
+// , faSass, faBootstrap, faGit, faNpm, faJs, faReact, faVuejs, faAngular, faFont, faFileCode, 
 function Resume() {
   return (
     
